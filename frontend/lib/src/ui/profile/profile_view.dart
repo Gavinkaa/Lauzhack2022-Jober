649ac7b4fc8611fb802cd44d576ec3c1119b1f65
Ludovic Burnier <ludovic.burnier@epfl.ch>
2022-12-03 20:10:47 +0100

    Proof of concept page to allow sign(up/in/out) to supa

--- frontend/lib/src/ui/profile/profile_view.dart
+++ frontend/lib/src/ui/profile/profile_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jober/src/models/repositories/auth_repository.dart';
 import 'package:jober/src/ui/profile/profile_viewmodel.dart';
 
 class ProfileView extends StatelessWidget {
@@ -8,6 +9,52 @@ class ProfileView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () async {
+                AuthRepository authRepository = AuthRepository.getInstance();
+                await authRepository.signUp(
+                    'supabase_ICI_CA_TEST@myburnier.ch', '12345678901');
+              },
+              child: Text("SIGN UP"),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                AuthRepository authRepository = AuthRepository.getInstance();
+                await authRepository.signUp('douglas.bouchet@epfl.ch', '321');
+              },
+              child: Text("SIGN UP - small password"),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                AuthRepository authRepository = AuthRepository.getInstance();
+                await authRepository.signIn(
+                    'supabase_ICI_CA_TEST@myburnier.ch', '12345678901');
+              },
+              child: Text("SIGN IN"),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                AuthRepository authRepository = AuthRepository.getInstance();
+                await authRepository.signIn(
+                    'supabase_ICI_CA_TEST@myburnier.ch', '12345678900');
+              },
+              child: Text("SIGN IN - wrong password"),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                AuthRepository authRepository = AuthRepository.getInstance();
+                await authRepository.signOut();
+              },
+              child: Text("SIGN OUT"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- frontend/lib/src/ui/profile/profile_viewmodel.dart
+++ frontend/lib/src/ui/profile/profile_viewmodel.dart
@@ -1,1 +1,19 @@
-class ProfileViewModel {}
+import 'package:flutter/material.dart';
+import 'package:jober/src/models/repositories/auth_repository.dart';
+
+class ProfileViewModel extends ChangeNotifier {
+  // User? _user;
+
+  // User? get user => _user;
+
+  // Future<void> signOut() async {
+  //   await AuthRepository.getInstance().signOut();
+  //   _user = null;
+  //   notifyListeners();
+  // }
+
+  // Future<void> getUser() async {
+  //   _user = await AuthRepository.getInstance().getUser();
+  //   notifyListeners();
+  // }
+}
