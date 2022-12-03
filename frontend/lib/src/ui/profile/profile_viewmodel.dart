f00872f416775ddf7cea5b3038e6e287f1292862
Ludovic Burnier <ludovic.burnier@epfl.ch>
2022-12-03 21:46:51 +0100

    sign up/in/out working with the structure

--- frontend/lib/src/ui/profile/profile_viewmodel.dart
+++ frontend/lib/src/ui/profile/profile_viewmodel.dart
@@ -1,19 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:jober/src/models/data/user_profile.dart';
 import 'package:jober/src/models/repositories/auth_repository.dart';
 
 class ProfileViewModel extends ChangeNotifier {
-  // User? _user;
+  UserProfile? _userProfile;
 
-  // User? get user => _user;
+  final _authRepository = AuthRepository.getInstance();
 
-  // Future<void> signOut() async {
-  //   await AuthRepository.getInstance().signOut();
-  //   _user = null;
-  //   notifyListeners();
-  // }
+  bool isConnected() {
+    // not connected if authRepository user is null
+    if (_authRepository.user == null) {
+      return false;
+    } else {
+      if (_userProfile == null) {
+        getUser();
+      }
+      return true;
+    }
+  }
 
-  // Future<void> getUser() async {
-  //   _user = await AuthRepository.getInstance().getUser();
-  //   notifyListeners();
-  // }
+  Future<void> signUp(String email, String password) async {
+    await _authRepository.signUp(email, password);
+    notifyListeners();
+  }
+
+  Future<void> signIn(String email, String password) async {
+    await _authRepository.signIn(email, password);
+    notifyListeners();
+  }
+
+  Future<void> signOut() async {
+    await _authRepository.signOut();
+    _userProfile = null;
+    notifyListeners();
+  }
+
+  Future<void> getUser() async {
+    _userProfile = await _authRepository.getUser();
+    notifyListeners();
+  }
 }
