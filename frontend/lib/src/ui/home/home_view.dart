486d20a9324d9aca6ee5b695f27d36fd8478f5e8
Simon Braillard <simon.braillard@gmail.com>
2022-12-03 15:23:37 +0100

    MVVM Project structure

---
+++ frontend/lib/src/ui/home/home_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:jober/src/ui/home/home_viewmodel.dart';
+
+class HomeView extends StatelessWidget {
+  HomeView({Key? key}) : super(key: key);
+  final HomeViewModel _viewModel = HomeViewModel();
+  static const routeName = "home";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
